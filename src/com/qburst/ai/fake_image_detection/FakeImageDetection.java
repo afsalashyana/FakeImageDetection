edac07991fd44714a015e37c9125b262dac95b2d
Muhammed Afsal Villan <afsalashyana@gmail.com>
2016-07-23 09:05:50 +0530

    Working On User Interface

---
+++ src/com/qburst/ai/fake_image_detection/FakeImageDetection.java
@@ -0,0 +1,27 @@
+package com.qburst.ai.fake_image_detection;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+
+public class FakeImageDetection extends Application {
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/resources/fxml/main_window.fxml"));
+        
+        Scene scene = new Scene(root);
+        
+        stage.setTitle("Christopher");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
---
+++ src/com/qburst/ai/fake_image_detection/controllers/Main_window_controller.java
@@ -0,0 +1,20 @@
+
+package com.qburst.ai.fake_image_detection.controllers;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+
+public class Main_window_controller implements Initializable {
+  
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}

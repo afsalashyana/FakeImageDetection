4a429265aa4c55874a904e55e4aef48208b3d048
Afsal <afsalashyana@gmail.com>
2016-08-03 16:50:58 +0530

    Final Build

--- src/com/qburst/fakeimagedetection/core/constants/ConstantObjects.java
+++ src/com/qburst/fakeimagedetection/core/constants/ConstantObjects.java
@@ -4,5 +4,6 @@ public class ConstantObjects {
 
     public static String[] supportedExtensions
             = {"JPG", "jpg", "JPEG", "PNG", "png", "TIFF", "TIF", "tif"};
-    public static String neuralNetworkPath = "nnet/MLPV2.0.nnet";
+//    public static String neuralNetworkPath = "nnet/MLPV2.0.nnet";
+    public static String neuralNetworkPath = "/home/qbuser/NeurophProjects/July26/Neural Networks/MLP4.nnet";
 }
